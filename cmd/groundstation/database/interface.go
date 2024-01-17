e8bc5a4618b37e0a4e995dfe34414648b29d03ee
Daniel G <code@danielg0.com>
2024-01-17 18:10:15 +0000

    Define database abstraction layer interface
    Define the set of functions that each database provider (ie. postgres
    and the mocking implementation for tests) implements. Define the bare
    essentials for now, we can go back and add more in future as needed.

---
+++ cmd/groundstation/database/interface.go
@@ -0,0 +1,16 @@
+package database
+
+import "github.com/gpuctl/gpuctl/internal/status"
+
+// define set of operations on the database that any provider will implement
+type Database interface {
+	// initialise the database
+	Initialise() error
+
+	// add the info from a heartbeat packet to the data store
+	// that machine may have not been seen before
+	Heartbeat(packet status.GPUStatusPacket) error
+
+	// get the latest metrics for all approved machines
+	AllMachines() ([]status.GPUStatusPacket, error)
+}
