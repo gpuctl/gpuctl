f3b6173c06c32ea57c95edc96f74ceee0a1d9dc2
Jyry Hjelt <jh2821@ic.ac.uk>
2024-01-17 13:09:14 +0000

    Added nice main loop to print out GPU status

--- cmd/satellite/main.go
+++ cmd/satellite/main.go
@@ -1,24 +1,28 @@
 package main
 
 import (
-	"fmt"
-	"github.com/gpuctl/gpuctl/internal/status/handlers"
 	"encoding/json"
+	"fmt"
+	"time"
+	"github.com/gpuctl/gpuctl/internal/status/handlers"
 )
 
 func main() {
 	// Just get the GPU data and print it in JSON
 	gpuHandler := handlers.NvidiaGPUHandler{}
-	res, err := gpuHandler.GetGPUStatus()
-	if err != nil {
-		fmt.Printf("Failed to parse: %v\n", err)
-		return
-	}
+	for {
+		res, err := gpuHandler.GetGPUStatus()
+		if err != nil {
+			fmt.Printf("Failed to parse: %v\n", err)
+			return
+		}
 
-	ser, err := json.Marshal(res);
-	if err != nil {
-		fmt.Printf("Failed to parse: %v\n", err)
-		return
+		ser, err := json.Marshal(res)
+		if err != nil {
+			fmt.Printf("Failed to parse: %v\n", err)
+			return
+		}
+		fmt.Printf("%s\n", ser)
+		time.Sleep(5 * time.Second)
 	}
-	fmt.Printf("%s\n", ser)
 }
