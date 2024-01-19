1172c268350d00706b73fd1e157bd2b961e51f42
Daniel G <code@danielg0.com>
2024-01-19 13:45:41 +0000

    Second draft of interface
    Following discussions, rewrite database interface to provide actions
    (eg. updating the last seen entry for a machine, appending a data point,
    getting the latest data), rather than tying them to particular requests.
    
    In future, we may want to extend LatestData() to support getting the
    data from a subset of machines by giving it a filter argument.

--- cmd/groundstation/database/interface.go
+++ cmd/groundstation/database/interface.go
@@ -4,10 +4,12 @@ import "github.com/gpuctl/gpuctl/internal/status"
 
 // define set of operations on the database that any provider will implement
 type Database interface {
-	// add the info from a heartbeat packet to the data store
-	// that machine may have not been seen before
-	Heartbeat(packet status.GPUStatusPacket) error
+	// update the last seen time for a satellite to the current time
+	UpdateLastSeen(host String) error
+
+	// record a new data point for a satellite in the data store
+	AppendDataPoint(packet status.GPUStatusPacket) error
 
 	// get the latest metrics for all approved machines
-	AllMachines() ([]status.GPUStatusPacket, error)
+	LatestData() ([]status.GPUStatusPacket, error)
 }
