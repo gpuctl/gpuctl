8c979e3fbdf42bc65a3b2a360711b12b49cb3b10
Daniel G <code@danielg0.com>
2024-01-19 15:51:01 +0000

    Fix spelling error in type

--- cmd/groundstation/database/interface.go
+++ cmd/groundstation/database/interface.go
@@ -5,7 +5,7 @@ import "github.com/gpuctl/gpuctl/internal/status"
 // define set of operations on the database that any provider will implement
 type Database interface {
 	// update the last seen time for a satellite to the current time
-	UpdateLastSeen(host String) error
+	UpdateLastSeen(host string) error
 
 	// record a new data point for a satellite in the data store
 	AppendDataPoint(packet status.GPUStatusPacket) error
