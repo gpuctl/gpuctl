007a69fb38f79ffc20fa11ceec455bd7b0c4b454
Nathaniel Burke <nrb21@ic.ac.uk>
2024-01-23 13:51:19 +0000

    Cook up some UI
    The tabs for each GPU should be cards holding only the most important
    info.
    
    We need to implement functionality to allow the user to click the cards
    and see a more detailed view

--- frontend/src/Components/WorkstationTab.tsx
+++ frontend/src/Components/WorkstationTab.tsx
@@ -1,18 +1,22 @@
-import { Box, Heading, useColorModeValue } from "@chakra-ui/react";
+import { Box, Center, Heading, useColorModeValue } from "@chakra-ui/react";
 import { WorkStationData } from "../App";
 
 export const WorkstationTab = ({ name, gpus }: WorkStationData) => {
   const textCol = useColorModeValue("black", "white");
   return (
-    <Box rounded={"md"} bg={useColorModeValue("white", "gray.900")}>
-      <Heading color={textCol}>{name}</Heading>
-      {gpus.map((s, i) => {
-        return (
-          <Box key={i}>
-            <Heading color={textCol}>{s.gpu_name}</Heading>
-          </Box>
-        );
-      })}
-    </Box>
+    <Center>
+      <Box w="100%" rounded={"md"} bg={useColorModeValue("white", "gray.900")}>
+        <Heading color={textCol}>{name}</Heading>
+        {gpus.map((s, i) => {
+          return (
+            <Box key={i}>
+              <Heading size="lg" color={textCol}>
+                {s.gpu_name}
+              </Heading>
+            </Box>
+          );
+        })}
+      </Box>
+    </Center>
   );
 };
