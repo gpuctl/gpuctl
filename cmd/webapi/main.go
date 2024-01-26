d1725e3e559b4b38729666bf57def141539120b9
Alona Enraght-Moony <code@alona.page>
2024-01-26 00:49:29 +0000

    webapi: Use `femto`

--- cmd/webapi/main.go
+++ cmd/webapi/main.go
@@ -1,36 +1,16 @@
 package main
 
 import (
-	"fmt"
-	"log"
+	"log/slog"
 	"net/http"
+
+	"github.com/gpuctl/gpuctl/internal/webapi"
 )
 
-const ALL_STATS string = `[{"name":"Shared","workStations":[{"name":"Workstation 1","gpus":[{"gpu_name":"NVIDIA GeForce GT 1030","gpu_brand":"GeForce","driver_ver":"535.146.02","memory_total":2048,"memory_util":0,"gpu_util":0,"memory_used":82,"fan_speed":35,"gpu_temp":31}]},{"name":"Workstation 2","gpus":[{"gpu_name":"NVIDIA TITAN Xp","gpu_brand":"Titan","driver_ver":"535.146.02","memory_total":12288,"memory_util":0,"gpu_util":0,"memory_used":83,"fan_speed":23,"gpu_temp":32},{"gpu_name":"NVIDIA TITAN Xp","gpu_brand":"Titan","driver_ver":"535.146.02","memory_total":12288,"memory_util":0,"gpu_util":0,"memory_used":83,"fan_speed":23,"gpu_temp":32}]},{"name":"Workstation 3","gpus":[{"gpu_name":"NVIDIA GeForce GT 730","gpu_brand":"GeForce","driver_ver":"470.223.02","memory_total":2001,"memory_util":0,"gpu_util":0,"memory_used":220,"fan_speed":30,"gpu_temp":27}]},{"name":"Workstation 5","gpus":[{"gpu_name":"NVIDIA TITAN Xp","gpu_brand":"Titan","driver_ver":"535.146.02","memory_total":12288,"memory_util":0,"gpu_util":0,"memory_used":83,"fan_speed":23,"gpu_temp":32},{"gpu_name":"NVIDIA TITAN Xp","gpu_brand":"Titan","driver_ver":"535.146.02","memory_total":12288,"memory_util":0,"gpu_util":0,"memory_used":83,"fan_speed":23,"gpu_temp":32}]},{"name":"Workstation 4","gpus":[{"gpu_name":"NVIDIA GeForce GT 1030","gpu_brand":"GeForce","driver_ver":"535.146.02","memory_total":2048,"memory_util":0,"gpu_util":0,"memory_used":82,"fan_speed":35,"gpu_temp":31}]},{"name":"Workstation 6","gpus":[{"gpu_name":"NVIDIA GeForce GT 730","gpu_brand":"GeForce","driver_ver":"470.223.02","memory_total":2001,"memory_util":0,"gpu_util":0,"memory_used":220,"fan_speed":30,"gpu_temp":27}]}]}]`
-
-const API_URL string = "localhost:8000"
-
 func main() {
-	init_server()
-	for {
-		// Loop
-	}
-}
+	slog.Info("Starting webapi")
 
-func init_server() {
-	fmt.Println("API is starting up!")
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(API_URL, nil))
-}
+	srv := webapi.NewServer()
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	switch r.URL.Path {
-	case "/api/stats/all":
-		fmt.Fprint(w, ALL_STATS)
-	default:
-		{
-
-		}
-	}
+	http.ListenAndServe(":8000", srv)
 }
--- internal/webapi/server.go
+++ internal/webapi/server.go
@@ -1,1 +1,72 @@
 package webapi
+
+import (
+	"log/slog"
+	"net/http"
+
+	"github.com/gpuctl/gpuctl/internal/femto"
+	"github.com/gpuctl/gpuctl/internal/uplink"
+)
+
+type Server struct {
+	mux *femto.Femto
+	api *api
+}
+
+type api struct {
+	// ???
+}
+
+func NewServer() *Server {
+	mux := new(femto.Femto)
+	api := &api{}
+
+	femto.OnGet[workstations](mux, "/api/stats/all", api.allstats)
+
+	return &Server{mux, api}
+}
+
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	s.mux.ServeHTTP(w, r)
+}
+
+var dummyData workstations = workstations{
+	workstationGroup{
+		Name: "Shared", WorkStations: []workStationData{
+			{
+				Name: "Workstation 1", Gpus: []uplink.GPUStats{
+					{Name: "NVIDIA GeForce GT 1030", Brand: "GeForce", DriverVersion: "535.146.02", MemoryTotal: 0x800, MemoryUtilisation: 0, GPUUtilisation: 0, MemoryUsed: 82, FanSpeed: 35, Temp: 31},
+				},
+			},
+			{
+				Name: "Workstation 2", Gpus: []uplink.GPUStats{
+					{Name: "NVIDIA TITAN Xp", Brand: "Titan", DriverVersion: "535.146.02", MemoryTotal: 0x3000, MemoryUtilisation: 0, GPUUtilisation: 0, MemoryUsed: 83, FanSpeed: 23, Temp: 32},
+					{Name: "NVIDIA TITAN Xp", Brand: "Titan", DriverVersion: "535.146.02", MemoryTotal: 0x3000, MemoryUtilisation: 0, GPUUtilisation: 0, MemoryUsed: 83, FanSpeed: 23, Temp: 32},
+				},
+			},
+			{
+				Name: "Workstation 3", Gpus: []uplink.GPUStats{
+					{Name: "NVIDIA GeForce GT 730", Brand: "GeForce", DriverVersion: "470.223.02", MemoryTotal: 0x7d1, MemoryUtilisation: 0, GPUUtilisation: 0, MemoryUsed: 220, FanSpeed: 30, Temp: 27},
+				},
+			},
+			{
+				Name: "Workstation 5", Gpus: []uplink.GPUStats{
+					{Name: "NVIDIA TITAN Xp", Brand: "Titan", DriverVersion: "535.146.02", MemoryTotal: 0x3000, MemoryUtilisation: 0, GPUUtilisation: 0, MemoryUsed: 83, FanSpeed: 23, Temp: 32},
+					{Name: "NVIDIA TITAN Xp", Brand: "Titan", DriverVersion: "535.146.02", MemoryTotal: 0x3000, MemoryUtilisation: 0, GPUUtilisation: 0, MemoryUsed: 83, FanSpeed: 23, Temp: 32},
+				},
+			},
+			{
+				Name: "Workstation 4", Gpus: []uplink.GPUStats{
+					{Name: "NVIDIA GeForce GT 1030", Brand: "GeForce", DriverVersion: "535.146.02", MemoryTotal: 0x800, MemoryUtilisation: 0, GPUUtilisation: 0, MemoryUsed: 82, FanSpeed: 35, Temp: 31},
+				},
+			},
+			{
+				Name: "Workstation 6", Gpus: []uplink.GPUStats{
+					{Name: "NVIDIA GeForce GT 730", Brand: "GeForce", DriverVersion: "470.223.02", MemoryTotal: 0x7d1, MemoryUtilisation: 0, GPUUtilisation: 0, MemoryUsed: 220, FanSpeed: 30, Temp: 27},
+				},
+			},
+		}}}
+
+func (a *api) allstats(r *http.Request, l *slog.Logger) (workstations, error) {
+	return dummyData, nil
+}
