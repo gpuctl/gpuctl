3f72a1714abf008075bb3d3289368d8a5abd5ed9
Alona Enraght-Moony <code@alona.page>
2024-01-26 02:34:08 +0000

    satellite: Add option to produce fake data.
    This means we can run locally, without needing a NVidia GPU.

---
+++ internal/gpustats/fake.go
@@ -0,0 +1,25 @@
+package gpustats
+
+import (
+	"math/rand"
+
+	"github.com/gpuctl/gpuctl/internal/uplink"
+)
+
+type FakeGPU struct{}
+
+func (FakeGPU) GPUStats() (uplink.GPUStats, error) {
+
+	return uplink.GPUStats{
+		Name:          "GPU-inator",
+		Brand:         "doofenshmirtz evil inc",
+		DriverVersion: "3.141592",
+		MemoryTotal:   1,
+
+		MemoryUtilisation: rand.Float64(),
+		GPUUtilisation:    rand.Float64(),
+		MemoryUsed:        rand.Float64() * 2000,
+		Temp:              rand.Float64()*40 + 20,
+		FanSpeed:          rand.Float64(),
+	}, nil
+}
