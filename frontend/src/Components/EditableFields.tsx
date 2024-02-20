d3a7ceb5b9cb76edeacff1d753d49508378292cc
Mila Sager <milaaa.sager@gmail.com>
2024-02-20 19:51:30 +0000

    Admin Page Refactor

---
+++ frontend/src/Components/EditableFields.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import {
+  Button,
+  Editable,
+  EditableInput,
+  EditablePreview,
+  Td,
+  Tr,
+} from "@chakra-ui/react";
+import { useModifyInfo, useRemoveMachine } from "../Hooks/hooks";
+import { WorkStationData } from "../Data";
+
+type EditableFieldProps = {
+  workstation: WorkStationData;
+  fieldKey: "cpu" | "motherboard" | "notes" | "group";
+  placeholder: string;
+};
+
+export function EditableField({
+  workstation, fieldKey, placeholder
+}: EditableFieldProps) {
+  const pickCol = (value: string) => (value ? "gray.600" : "gray.300");
+  const modifyInfo = useModifyInfo();
+
+  const handleSubmit = (newValue: string) => {
+    modifyInfo(workstation.name, {
+      group: fieldKey == "group" ? newValue : null,
+      motherboard: fieldKey === "motherboard" ? newValue : null,
+      cpu: fieldKey === "cpu" ? newValue : null,
+      notes: fieldKey === "notes" ? newValue : null,
+    });
+  };
+
+  return (
+    <Td>
+      <Editable
+        defaultValue={workstation[fieldKey as keyof WorkStationData] as string}
+        placeholder={placeholder}
+        textColor={pickCol(workstation[fieldKey as keyof WorkStationData] as string)}
+        onSubmit={handleSubmit}
+      >
+        <EditablePreview />
+        <EditableInput />
+      </Editable>
+    </Td>
+  );
+}
---
+++ frontend/src/Components/GroupInfoManagement.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Box, Button, Heading, Table, TableContainer, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react';
+import { EditableField } from './EditableFields';
+import { useRemoveMachine } from '../Hooks/hooks';
+import { WorkStationGroup } from '../Data';
+
+type GroupInfoManagementProps = {
+  groups: WorkStationGroup[];
+};
+
+export const GroupInfoManagement: React.FC<GroupInfoManagementProps> = ({ groups }) => {
+  const removeMachine = useRemoveMachine();
+
+  return (
+    <Box w="100%">
+      <Heading size="lg">Group & Info Management:</Heading>
+      <TableContainer mt={4}>
+        <Table variant="striped">
+          <Thead>
+            <Tr>
+              <Th>Hostname</Th>
+              <Th>Group</Th>
+              <Th>CPU</Th>
+              <Th>Motherboard</Th>
+              <Th>Notes</Th>
+              <Th>Action</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {groups.flatMap((group) =>
+              group.workstations.map((workstation) => (
+                <Tr key={workstation.name}>
+                  <Td>{workstation.name}</Td>
+                  <EditableField workstation={workstation} fieldKey="group" placeholder='unknown' />
+                  <EditableField workstation={workstation} fieldKey="cpu" placeholder='unknown'/>
+                  <EditableField workstation={workstation} fieldKey="motherboard" placeholder='unknown'/>
+                  <EditableField workstation={workstation} fieldKey="notes" placeholder='none'/>
+                  <Td>
+                    <Button colorScheme="red" onClick={() => removeMachine(workstation.name)}>Remove</Button>
+                  </Td>
+                </Tr>
+              ))
+            )}
+          </Tbody>
+        </Table>
+      </TableContainer>
+    </Box>
+  );
+};
