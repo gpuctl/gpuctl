16f23334704c6e7ee3c0510ede7fd06a71c938f7
Jyry Hjelt <jh2821@ic.ac.uk>
2024-02-21 20:01:22 +0000

    gofmt strikes again

--- internal/webapi/server_test.go
+++ internal/webapi/server_test.go
@@ -286,7 +286,7 @@ func TestServerEndpoints(t *testing.T) {
 			method:         http.MethodPost,
 			endpoint:       "/api/admin/remove_file",
 			expectedStatus: http.StatusUnauthorized,
-			body:			[]byte(`{"hostname":"bogus", "filename":"bogus"}`),
+			body:           []byte(`{"hostname":"bogus", "filename":"bogus"}`),
 			headers:        map[string]string{"Cookie": "token=wrongtoken"},
 		},
 	}
