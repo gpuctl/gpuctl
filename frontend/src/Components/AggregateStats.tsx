baba6f93bd8949d60b65d6478126410381022e3f
Jyry Hjelt <jh2821@ic.ac.uk>
2024-03-07 14:14:28 +0000

    fix aggregate data to be the correct format

--- frontend/src/Components/AggregateStats.tsx
+++ frontend/src/Components/AggregateStats.tsx
@@ -3,14 +3,14 @@ import { VStack, Heading, UnorderedList, ListItem } from "@chakra-ui/react";
 export const AggregateStats = ({
   data,
 }: {
-  data: { average_use: number; uptime: string };
+  data: { percent_used: number; total_energy: number };
 }) => {
   return (
     <VStack paddingLeft={5} spacing={5} align="left">
       <Heading size="lg">Statistics</Heading>
       <UnorderedList spacing={3}>
-        <ListItem>Average Usage: {data.average_use}%</ListItem>
-        <ListItem>Uptime: {data.uptime}</ListItem>
+        <ListItem>Average Usage: {data.percent_used}%</ListItem>
+        <ListItem>Uptime: {data.total_energy}</ListItem>
       </UnorderedList>
     </VStack>
   );
