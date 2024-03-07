582393691ba610b06a311d4c7a232d520f132301
Jacob Bailey <jab221@ic.ac.uk>
2024-03-07 13:23:30 +0000

    Only change one button to show copied

--- frontend/src/Components/GroupInfoManagement.tsx
+++ frontend/src/Components/GroupInfoManagement.tsx
@@ -267,7 +267,7 @@ export const GroupInfoManagement = ({
                           onClick={() => handleShutdownClick(workstation.name)}
                           disabled={copied}
                         >
-                          {copied ? "Copied" : "Copy Shutdown Command"}
+                          {copied && workstation.name === currentMachine ? "Copied" : "Copy Shutdown Command"}
                         </Button>
                       </Td>
                       <Td>
